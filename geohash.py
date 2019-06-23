94c5b565fc465eec9568c87b2ad55e75c5f3066b
clarkschaefer <clark.h.schaefer@gmail.com>
2019-06-23 17:55:53 -0500

    Made script runnable without "python3" in front... Generalized important stuff to use datetime.date and isoformat() Made functions have fewer default parameters Made geohash function more readable Added argparsing, so it can be used as a CLI tool now

--- geohash.py
+++ geohash.py
@@ -1,12 +1,14 @@
+#!/usr/bin/env python3
 """
 Created on Tue Jun  4 15:18:03 2019
 
 @author: clarkhs
 """
-# import argparse
+import argparse
 import datetime
+import dateutil.parser
 import hashlib
-import webbrowser
+# import webbrowser
 
 from math import trunc
 
@@ -38,14 +40,14 @@ def get_dji_data(api_key=AV_API_KEY):
               'function': 'TIME_SERIES_DAILY',
               'symbol': "DJI"}
     url = 'https://www.alphavantage.co/query?'
+
     response = requests.get(url, params)
     dji_data_json = response.json()['Time Series (Daily)']
 
     return dji_data_json
 
 
-def select_dji_data_on_date(dji_data=get_dji_data(),
-                            request_date=TODAYS_DATE):
+def select_dji_data_on_date(dji_data, request_date=TODAYS_DATE):
     '''
     Get the specific most recent opening, given a date.
     Note that it doesn't always return the most up-to-date data,
@@ -56,15 +58,16 @@ def select_dji_data_on_date(dji_data=get_dji_data(),
     dji_data_oneday = None
 
     while dji_data_oneday is None and attempts < 7:
-        date = next(day_gen)
-        if date.weekday() > 4:
+        selected_date = next(day_gen)
+        print(selected_date)
+        if selected_date.weekday() > 4:
             pass
         else:
             try:
-                dji_data_oneday = dji_data[date.isoformat()]
+                dji_data_oneday = dji_data[selected_date.isoformat()]
             except KeyError:
                 raise NotYetAvailable('DJI open data not available on',
-                                      TODAYS_DATE.isoformat())
+                                      selected_date.isoformat())
 
     return dji_data_oneday
 
@@ -89,6 +92,7 @@ def get_latlong_by_ip():
     This only exists so that I can make it look neater down below.
     '''
     url = 'http://ipinfo.io/json'
+
     response = requests.get(url)
 
     loc = [float(s) for s in response.json()['loc'].split(',')]
@@ -106,29 +110,28 @@ def get_first_matching_key(search_dictionary, search_key):
     return desired_value
 
 
-def geohash(selected_date=TODAYS_DATE, dji_open=None,
-            loc=get_latlong_by_ip()):
+def geohash(location, selected_date=TODAYS_DATE, dji_open=None):
     '''
     Think from antigravity import geohash
     but like better or something.
     '''
-    loc = [trunc(s) for s in loc]
-    hash_input = selected_date+'-'+str(round(dji_open, 2))
+    location = [trunc(int(f)) for f in location]
+    hash_input = '-'.join([selected_date.isoformat(), str(round(dji_open, 2))])
     hash_output = hashlib.md5(bytes(hash_input, 'utf-8'))
     hex_output = hash_output.hexdigest()
 
     loc_hash = [hex_output[:len(hex_output)//2],
                 hex_output[len(hex_output)//2:]]
-    fractions = [int(s, 16)/2**64 for s in loc_hash]
-    final_loc = [loc[0]+fractions[0]*loc[0]/abs(loc[0]),
-                 loc[1]+fractions[1]*loc[1]/abs(loc[1])]
+    fractions = [int(s, 16) / 2 ** 64 for s in loc_hash]
+    final_loc = [location[0] + fractions[0] * location[0] / abs(location[0]),
+                 location[1] + fractions[1] * location[1] / abs(location[1])]
 
     return final_loc, fractions
 
 
-def main():
+def main(args):
     '''
-    At runtiem, goes and finds the most recent DJI open.
+    At runtime, goes and finds the most recent DJI open.
     Then, gets your approximate location from your IP,
     and truncates that to get just the integers.
     It picks out most recent DJI open for the day,
@@ -137,16 +140,29 @@ def main():
     prints all the regular useful information,
     and opens a webpage to that location.
     '''
-    dji_data = get_dji_data()
-    dji_data_oneday = select_dji_data_on_date(dji_data, TODAYS_DATE)
-    location = [trunc(s) for s in get_latlong_by_ip()]
+    if args.location is None:
+        location = [trunc(s) for s in get_latlong_by_ip()]
+    else:
+        location = args.location
 
-    dji_open = round(float(get_first_matching_key(dji_data_oneday, "open")), 2)
+    if args.date is None:
+        date = TODAYS_DATE
+    else:
+        date = dateutil.parser.parse(args.date).date()
 
-    goal_loc, fractions = geohash(TODAYS_DATE.isoformat(), dji_open, location)
+    if args.dji_open is None:
+        dji_data = get_dji_data()
+        dji_data_oneday = select_dji_data_on_date(dji_data, date)
+
+        dji_open = round(float(get_first_matching_key(dji_data_oneday,
+                                                      "open")), 2)
+    else:
+        dji_open = args.dji_open
+
+    goal_loc, fractions = geohash(location, date, dji_open)
 
     print(location,
-          TODAYS_DATE.isoformat()+'-'+str(dji_open),
+          date.isoformat()+'-'+str(dji_open),
           fractions,
           goal_loc,
           sep='\n')
@@ -156,4 +172,14 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--location', '-l', nargs=2,
+                        metavar=('LAT', 'LONG'),
+                        help='Current latitude and longitude')
+    parser.add_argument('--date', '-d', metavar='DATE',
+                        help='''Date of desired geohash.
+                                Right now only works for last 100 days''')
+    parser.add_argument('--dji-open',
+                        help='Specify a DJI open if you like.')
+    args = parser.parse_args()
+    main(args)
